296f76b360a475bdc4f8f513262ba3f6ad341664
Abdullah Omer Mohammed <mabdul23@depaul.edu>
2025-05-04 12:33:34 -0500

    Initial Commit

---
+++ IWasHere/IWasHere/IWasHereApp.swift
@@ -0,0 +1,17 @@
+//
+//  IWasHereApp.swift
+//  IWasHere
+//
+//  Created by Abdullah Omer Mohammed on 5/4/25.
+//
+
+import SwiftUI
+
+@main
+struct IWasHereApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
